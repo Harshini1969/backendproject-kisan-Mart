ca1692dc8aa18c2e13d577751c35864e09e91a58
Harshini1969 <narahariharshini09@gmail.com>
2026-03-31 11:02:57 +0530

    adminCustomer curd operations

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { adminAuth } = require("../Middleware/auth");
-const { getCustomers } = require("../controllers/AdminControllers");
+const { getCustomers, deleteCustomer,updateCustomer} = require("../controllers/AdminControllers");
 
 const router = express.Router();
 
 router.get("/customers", adminAuth, getCustomers);
+router.delete("/customer/:id", deleteCustomer);
+router.put("/customer/:id", updateCustomer);
 
 module.exports = router;
--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -3,7 +3,6 @@ const UserModel = require("../model/UserModel");
 const getCustomers = async (req, res) => {
   try {
     const customers = await UserModel.find({ role: "customer" });
-
     res.json({
       customers
     });
@@ -14,6 +13,45 @@ const getCustomers = async (req, res) => {
   }
 };
 
+//  DELETE customer
+const deleteCustomer = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await UserModel.findByIdAndDelete(id);
+    res.json({
+      message: "Customer deleted successfully"
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error deleting customer"
+    });
+  }
+};
+
+// UPDATE customer
+const updateCustomer = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      id,
+      req.body,
+      //{ new: true }
+      {returnDocument: after}
+    );
+
+    res.json({
+      message: "Customer updated successfully",
+      customer: updatedUser
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error updating customer"
+    });
+  }
+};
+
 module.exports = {
-  getCustomers
+  getCustomers,
+  deleteCustomer,
+  updateCustomer
 };
