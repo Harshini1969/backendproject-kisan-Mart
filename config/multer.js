c537191e5bfcb5308b017ef789e6a9e1e9e70207
harshini1969 <nnarahariharshini09@gmail.com>
2026-04-14 11:19:58 +0530

    config

--- config/multer.js
+++ config/multer.js
@@ -1,4 +1,6 @@
 const multer = require("multer")
+const path = require("path");
+const uploadPath = path.join(__dirname, "../uploads");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log(" RECEIVED FIELD:", file.fieldname);
